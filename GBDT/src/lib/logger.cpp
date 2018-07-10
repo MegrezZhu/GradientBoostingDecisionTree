f61b612ca4b461d016230a380d4e018484873b23
MegrezZhu <mystery490815101@gmail.com>
2018-07-10 19:37:31 +0800

    :wink: fixed some warnings

--- GBDT/src/lib/logger.cpp
+++ GBDT/src/lib/logger.cpp
@@ -1,3 +1,5 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include "logger.h"
 
 using namespace std;
--- GBDT/src/lib/util.cpp
+++ GBDT/src/lib/util.cpp
@@ -1,3 +1,5 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <fstream>
 #include <algorithm>
 #include <cctype>
